1cb516d709b82532c8fc312df41ac89436a1b475
StudioPF <frassepierrefrancois@gmail.com>
2026-07-15 02:56:51 +0200

    Update script.js
    Signed-off-by: StudioPF <frassepierrefrancois@gmail.com>

--- js/script.js
+++ js/script.js
@@ -4725,64 +4725,7 @@ function scrollTotal(offset=100) {
     });
   }
 }
-function initMenuMobile() {
 
-    const menuToggle = document.getElementById("menu-toggle");
-    const menuMobile = document.getElementById("menu-contenu");
-    const overlay = document.getElementById("menu-overlay");
-
-
-    if (!menuToggle || !menuMobile || !overlay) {
-        console.warn("Menu mobile introuvable");
-        return;
-    }
-
-
-    menuToggle.addEventListener("click", () => {
-
-        menuMobile.classList.toggle("active");
-        overlay.classList.toggle("active");
-
-    });
-
-
-    overlay.addEventListener("click", () => {
-
-        menuMobile.classList.remove("active");
-        overlay.classList.remove("active");
-
-    });
-
-
-    document.querySelectorAll(".menu-mobile-list a")
-    .forEach(link => {
-
-        link.addEventListener("click", () => {
-
-            menuMobile.classList.remove("active");
-            overlay.classList.remove("active");
-
-        });
-
-    });
-
-}
-
-    // Fermeture après clic sur un lien du menu
-    const menuLinks = menuMobile.querySelectorAll("a");
-
-    menuLinks.forEach(link => {
-
-        link.onclick = function () {
-
-            menuMobile.classList.remove("active");
-            menuOverlay.classList.remove("active");
-
-        };
-
-    });
-
-}
 function initThemeToggle() {
 
     const themeToggle = document.getElementById("themeToggle");
