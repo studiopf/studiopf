3debdb18b39f56010ecaa2ba6082e0f5c709b4d9
StudioPF <frassepierrefrancois@gmail.com>
2026-07-16 02:10:02 +0200

    Update script.js
    Signed-off-by: StudioPF <frassepierrefrancois@gmail.com>

--- js/script.js
+++ js/script.js
@@ -4983,10 +4983,11 @@ function appelimg() {
         'niv3/nivtrois16.png','niv3/nivtrois17.png','niv3/nivtrois18.png','niv3/nivtrois19.png','niv3/nivtrois20.png',
         'niv3/nivtrois21.png','niv3/nivtrois22.png','niv3/nivtrois23.png','niv3/nivtrois24.png','niv3/nivtrois25.png',
         'niv3/nivtrois26.png','niv3/nivtrois27.png','niv3/nivtrois28.png','niv3/nivtrois29.png','niv3/nivtrois30.png','niv3/nivtrois31.png','niv3/nivtrois32.png','niv3/nivtrois33.png','niv3/nivtrois34.png','niv3/nivtrois35.png','niv3/nivtrois36.png',
+        'niv3/nivtrois37.png',
         // Niveau Expo
         'niv4/nivquatre1.png','niv4/nivquatre2.png','niv4/nivquatre3.png','niv4/nivquatre4.png','niv4/nivquatre5.png',
         'niv4/nivquatre6.png','niv4/nivquatre7.png','niv4/nivquatre8.png','niv4/nivquatre9.png','niv4/nivquatre10.png','niv4/nivquatre11.png','niv4/nivquatre12.png','niv4/nivquatre13.png','niv4/nivquatre14.png','niv4/nivquatre15.png','niv4/nivquatre16.png','niv4/nivquatre17.png',
-        'niv4/nivquatre18.png','niv4/nivquatre19.png','niv4/nivquatre20.png',
+        'niv4/nivquatre18.png','niv4/nivquatre19.png',
         // Expo pure
         'expo/expo1.png','expo/expo2.png','expo/expo3.png','expo/expo4.png','expo/expo5.png',
         'expo/expo6.png','expo/expo7.png','expo/expo8.png','expo/expo9.png','expo/expo10.png','expo/expo11.png','expo/expo12.png'
